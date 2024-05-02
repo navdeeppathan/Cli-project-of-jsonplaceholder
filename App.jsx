e3aae99a10843462fd1a82d2e47d9f2be6de951f
Navdeep Pathan <navdeeppathan975@gmail.com>
2024-05-02 19:19:21 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+
+export default function App() {
+  
+  return (
+    <View >
+      
+    <HomeScreen/>
+    </View>
+  );
+}
+
+
